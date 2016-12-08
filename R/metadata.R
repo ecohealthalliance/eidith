adcef497dcd0f7a92feb1757e712f434e1d2be8b
Noam Ross <ross@ecohealthalliance.org>
2016-12-08 11:56:37 -0500

    Update metadata docs

--- R/metadata.R
+++ R/metadata.R
@@ -1,24 +1,27 @@
 #' EIDITH table metadata
 #'
 #' Descriptions of fields in the EIDITH database.  Running `ed_metadata()` will
-#' return a data frame with this information. \if{html}{This table is also shown below in a searchable and exportable format.}
+#' return a data frame with this information.
 #'
-#' More information can be found at the [EIDITH resources page](https://eidith.org/Resources.aspx).
+#' More information can be found at [the EIDITH resources page](https://eidith.org/Resources.aspx).
 #'
 #' \if{html}{
-#'
-#' Here "Original Name" refers to the name in the EIDITH database, "Processed Name"
-#' is what is found after running \code{\link{ed_process}} on the data tables.
-#'
-#'   \Sexpr[echo=FALSE, results=rd, stage=render]{
-#'     tmp <- paste0(system.file("", package="eidith"), "ed_meta_table.html")
+#'   \Sexpr[echo=FALSE, results=rd, stage=build]{
+#'   in_pkgdown <- any(grepl("as_html.tag_Sexpr", sapply(sys.calls(), function(a) paste(deparse(a), collapse = "\n"))))
+#'     if(in_pkgdown) {
+#'       text <- c('In RStudio, this help file includes a searchable table of values.')
+#'     } else {
+#'     tmp <- tempfile(fileext=".html")
 #'       htmlwidgets::saveWidget(DT::datatable(eidith::ed_metadata()), tmp)
-#'       return(paste(
-#'               '\\\out{<div style= "width:100\%"',
-#'                  paste(readLines(tmp), collapse="\n"),
-#'               '</div>}',
-#'               sep="\n"))
-#'   }
+#'       text <- paste('Below is a searchable version of this table.',
+#'       'Here "Original Name" refers to the name in the online EIDITH database, "Processed Name" is the name in the locally-stored tables after processing.',
+#'       '\\\out{<div style="width:100\%">',
+#'          paste(stringi::stri_subset_regex(readLines(tmp), "^</?(!DOCTYPE|meta|body|html)",negate=TRUE), collapse="\n"),
+#'       '</div>}',
+#'       sep="\n")
+#'     }
+#'     text
+#' }
 #' }
 #
 #' @rdname ed_metadata
