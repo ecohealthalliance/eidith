14569ee19999e7ac404b8a4e161735db84448b83
Noam Ross <ross@ecohealthalliance.org>
2016-12-09 18:25:35 -0500

    Update ed_metadata help

--- R/metadata.R
+++ R/metadata.R
@@ -6,7 +6,7 @@
 #' More information can be found at [the EIDITH resources page](https://eidith.org/Resources.aspx).
 #'
 #' \if{html}{
-#'   \Sexpr[echo=FALSE, results=rd, stage=build]{
+#'   \Sexpr[echo=FALSE, results=rd, stage=render]{
 #'   in_pkgdown <- any(grepl("as_html.tag_Sexpr", sapply(sys.calls(), function(a) paste(deparse(a), collapse = "\n"))))
 #'     if(in_pkgdown) {
 #'       text <- c('In RStudio, this help file includes a searchable table of values.')
@@ -14,7 +14,7 @@
 #'     tmp <- tempfile(fileext=".html")
 #'       htmlwidgets::saveWidget(DT::datatable(eidith::ed_metadata()), tmp)
 #'       text <- paste('Below is a searchable version of this table.',
-#'       'Here "Original Name" refers to the name in the online EIDITH database, "Processed Name" is the name in the locally-stored tables after processing.',
+#'       'Here \\\bold{original_name} refers to the name in the online EIDITH database. The variable name in the local database is \\\bold{auto_processed_name} or \\\bold{replacement_name}, if the latter exists. Variables dropped from the local database have "DROP" in \\\bold{replacement_name}. Where relevant \\\bold{processing_notes} describes transformations applied to the data in \\\code{ed_process()}.',
 #'       '\\\out{<div style="width:100\%">',
 #'          paste(stringi::stri_subset_regex(readLines(tmp), "^</?(!DOCTYPE|meta|body|html)",negate=TRUE), collapse="\n"),
 #'       '</div>}',
@@ -27,5 +27,5 @@
 #' @rdname ed_metadata
 #' @export
 ed_metadata <- function() {
- ed_metadata_
+  ed_metadata_
 }
