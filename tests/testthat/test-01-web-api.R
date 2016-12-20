f61e50e8d37aac249a95e945c30a3800d6bddbb9
Noam Ross <ross@ecohealthalliance.org>
2016-12-20 15:22:36 -0500

    Add database download to tests

--- tests/testthat/test-01-web-api.R
+++ tests/testthat/test-01-web-api.R
@@ -9,18 +9,22 @@ context("Web API")
 
 if(HAS_INTERNET && HAS_GLOBAL_CRED) {
 
-test_that("Server API has expected fields", {
-  for(endpt in eidith:::endpoints[1:5]) {
-    url <- modify_url(paste0(eidith:::eidith_base_url, endpt),
-                      query = list(header_only="y"))
-    headers = GET(url, authenticate(Sys.getenv("EIDITH_GLOBAL_USERNAME"), Sys.getenv("EIDITH_GLOBAL_PASSWORD"))) %>%
-      content() %>%
-      unlist()
-    expected_fields <- filter(ed_metadata(), endpoint==endpt) %>% use_series(original_name) %>% na.omit
-    expect_true(all(headers %in% expected_fields))
-    expect_true(all(expected_fields %in% headers))
-  }
-})
+  test_that("Server API has expected fields", {
+    for(endpt in eidith:::endpoints[1:5]) {
+      url <- modify_url(paste0(eidith:::eidith_base_url, endpt),
+                        query = list(header_only="y"))
+      headers = GET(url, authenticate(Sys.getenv("EIDITH_GLOBAL_USERNAME"), Sys.getenv("EIDITH_GLOBAL_PASSWORD"))) %>%
+        content() %>%
+        unlist()
+      expected_fields <- filter(ed_metadata(), endpoint==endpt) %>% use_series(original_name) %>% na.omit
+      expect_true(all(headers %in% expected_fields))
+      expect_true(all(expected_fields %in% headers))
+    }
+  })
+
+  test_that("Database download takes place", {
+    expect_success(ed_db_download())
+  })
 
 }
 
