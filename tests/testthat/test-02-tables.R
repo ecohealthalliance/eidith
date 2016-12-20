6400c7359db759cf27a555cdab0d62bbc16e68cc
Noam Ross <ross@ecohealthalliance.org>
2016-12-20 15:16:21 -0500

    Add test to check that database has right fields

--- tests/testthat/test-02-tables.R
+++ tests/testthat/test-02-tables.R
@@ -3,15 +3,26 @@ DB_EXISTS <- length(ed_db_status()) == 7
 context("Tables")
 
 if(!DB_EXISTS) {
-  message("Database empty, skipping data extract tests")
+  message("Database empty, some tests")
 } else {
 
-test_that("Table extracts work", {
-  expect_identical(ed_events(event_date == "2010-07-12"),
-                   dplyr::filter_(ed_events(), ~event_date=="2010-07-12"))
-  expect_identical(ed_animals(sex == "Female"),
-                   dplyr::filter_(ed_animals(), ~sex=="Female"))
-})
+  test_that("Table extracts work", {
+    expect_identical(ed_events(event_date == "2010-07-12"),
+                     dplyr::filter_(ed_events(), ~event_date=="2010-07-12"))
+    expect_identical(ed_animals(sex == "Female"),
+                     dplyr::filter_(ed_animals(), ~sex=="Female"))
+  })
 
-
+  test_that("Fields in database are as expected", {
+    for(tb in eidith:::db_tables[1:6]) {
+      ed_tb <- tbl(eidith_db(), tb) %>% head %>% collect
+      expected_fields <- filter(ed_metadata(), table == tb) %>%
+        mutate(nname = coalesce(replacement_name, auto_processed_name)) %>%
+        use_series(nname) %>%
+        na.omit() %>%
+        stringi::stri_subset_fixed("DROP", negate=TRUE)
+      expect_true(all(names(ed_tb %in% expected_fields)))
+      expect_true(all(names(expected_fields %in% ed_tb)))
+    }
+  })
 }
