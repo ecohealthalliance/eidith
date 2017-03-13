532cd678d63e3210a890ab761fcec9358498a388
Cale Basaraba <basaraba@ecohealthalliance.org>
2017-03-13 11:32:29 -0400

    vignettes

--- vignettes/preprocessing.R
+++
@@ -1,8 +0,0 @@
-## ---- setup, echo = FALSE, message = FALSE-------------------------------
-knitr::opts_chunk$set(
-  comment = "#>",
-  tidy = FALSE,
-  error = FALSE,
-  fig.width = 8,
-  fig.height = 8)
-
