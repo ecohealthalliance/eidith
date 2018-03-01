13a19f3693af04f23625411d4832e79455c48255
Cale Basaraba <basaraba@ecohealthalliance.org>
2018-03-01 15:22:00 -0500

    adding ?ed2_metadata help file / data-table and documentation

--- R/metadata.R
+++ R/metadata.R
@@ -1,4 +1,4 @@
-#' EIDITH PREDICT-1 & PREDICT-2 table metadata
+#' EIDITH PREDICT-1 table metadata
 #'
 #' Descriptions of fields in the EIDITH database.  Running `ed_metadata()` will
 #' return a data frame with this information. `ed_taxagroups` returns a table
@@ -33,7 +33,34 @@ ed_metadata <- function() {
   ed_metadata_
 }
 
-#' @rdname ed_metadata
+
+#' EIDITH PREDICT-1 table metadata
+#'
+#' Descriptions of fields in the EIDITH database.  Running `ed2_metadata()` will
+#' return a data frame with this information.
+#'
+#' More information can be found at [the EIDITH resources page](https://eidith.org/Resources.aspx).
+#'
+#' \if{html}{
+#'   \Sexpr[echo=FALSE, results=rd, stage=build]{
+#'   in_pkgdown <- any(grepl("as_html.tag_Sexpr", sapply(sys.calls(), function(a) paste(deparse(a), collapse = "\n"))))
+#'     if(in_pkgdown) {
+#'       mytext <- c('In RStudio, this help file includes a searchable table of values.')
+#'     } else {
+#'     tmp <- tempfile(fileext=".html")
+#'       htmlwidgets::saveWidget(DT::datatable(eidith::ed2_metadata()), tmp)
+#'       mytext <- paste('Below is a searchable version of this table.',
+#'       'Here \\\bold{original_name} refers to the name in the online EIDITH database. The variable name in the local database is \\\bold{auto_processed_name} or \\\bold{replacement_name}, if the latter exists. Variables dropped from the local database have "DROP" in \\\bold{replacement_name}. Where relevant \\\bold{processing_notes} describes transformations applied to the data in \\\code{ed_process()} or information regarding the type of variable. The \\\bold{question} column is relevant for Human Questionnaire tables, indication which question the data field comes from.',
+#'       '\\\out{<div style="width:100\%">',
+#'          paste(stringi::stri_subset_regex(readLines(tmp), "^</?(!DOCTYPE|meta|body|html)",negate=TRUE), collapse="\n"),
+#'       '</div>}',
+#'       sep="\n")
+#'     }
+#'     mytext
+#' }
+#' }
+#'
+#' @rdname ed2_metadata
 #' @export
 #' @importFrom DT datatable
 #' @importFrom htmlwidgets saveWidget
