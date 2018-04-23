8ed879eb2f6477311668ff432105a011a91e41ad
Noam Ross <noam.ross@gmail.com>
2018-04-23 12:17:20 -0400

    Make interactive metadata tables more robust

--- R/metadata.R
+++ R/metadata.R
@@ -12,19 +12,14 @@
 #'     if(in_pkgdown) {
 #'       mytext <- c('In RStudio, this help file includes a searchable table of values.')
 #'     } else {
-#'     tmp <- tempfile(fileext=".html")
-#'       htmlwidgets::saveWidget(DT::datatable(eidith::ed_metadata()), tmp)
-#'       mytext <- paste('Below is a searchable version of this table.',
-#'       'Here \\\bold{original_name} refers to the name in the online EIDITH database. The variable name in the local database is \\\bold{auto_processed_name} or \\\bold{replacement_name}, if the latter exists. Variables dropped from the local database have "DROP" in \\\bold{replacement_name}. Where relevant \\\bold{processing_notes} describes transformations applied to the data in \\\code{ed_process()}.',
-#'       '\\\out{<div style="width:100\%">',
-#'          paste(stringi::stri_subset_regex(readLines(tmp), "^</?(!DOCTYPE|meta|body|html)",negate=TRUE), collapse="\n"),
-#'       '</div>}',
-#'       sep="\n")
+#'       mytext <- eidith::rd_datatable(eidith::ed_metadata())
 #'     }
 #'     mytext
+#'   }
 #' }
-#' }
-#
+#'
+#' \if{text,latex}{The HTML version of this help file includes a searchable table of values.}
+#'
 #' @rdname ed_metadata
 #' @export
 #' @importFrom DT datatable
@@ -41,24 +36,20 @@ ed_metadata <- function() {
 #'
 #' More information can be found at [the EIDITH resources page](https://eidith.org/Resources.aspx).
 #'
+#'
 #' \if{html}{
 #'   \Sexpr[echo=FALSE, results=rd, stage=build]{
 #'   in_pkgdown <- any(grepl("as_html.tag_Sexpr", sapply(sys.calls(), function(a) paste(deparse(a), collapse = "\n"))))
 #'     if(in_pkgdown) {
 #'       mytext <- c('In RStudio, this help file includes a searchable table of values.')
 #'     } else {
-#'     tmp <- tempfile(fileext=".html")
-#'       htmlwidgets::saveWidget(DT::datatable(eidith::ed2_metadata()), tmp)
-#'       mytext <- paste('Below is a searchable version of this table.',
-#'       'Here \\\bold{original_name} refers to the name in the online EIDITH database. The variable name in the local database is \\\bold{auto_processed_name} or \\\bold{replacement_name}, if the latter exists. Variables dropped from the local database have "DROP" in \\\bold{replacement_name}. Where relevant \\\bold{processing_notes} describes transformations applied to the data in \\\code{ed_process()} or information regarding the type of variable. The \\\bold{question} column is relevant for Human Questionnaire tables, indication which question the data field comes from.',
-#'       '\\\out{<div style="width:100\%">',
-#'          paste(stringi::stri_subset_regex(readLines(tmp), "^</?(!DOCTYPE|meta|body|html)",negate=TRUE), collapse="\n"),
-#'       '</div>}',
-#'       sep="\n")
+#'       mytext <- eidith::rd_datatable(eidith::ed2_metadata())
 #'     }
 #'     mytext
+#'   }
 #' }
-#' }
+#'
+#' \if{text,latex}{The HTML version of this help file includes a searchable table of values.}
 #'
 #' @rdname ed2_metadata
 #' @export
@@ -103,4 +94,35 @@ ed2_update_metadata <- function(verbose = TRUE){
   )
 }
 
+
+#'@export
+#'@importFrom DT datatable
+#'@noRd
+rd_datatable <- function(df, width="100%", ...) {
+  wrap_widget(datatable(df, width=width, ...))
+}
+
+#'@export
+#'@importFrom stringi stri_subset_regex
+#'@importFrom htmlwidgets saveWidget
+#'@noRd
+wrap_widget <- function(widget) {
+  tmp <- tempfile(fileext=".html")
+  saveWidget(widget, tmp)
+  widg <- paste(stringi::stri_subset_regex(readLines(tmp), "^</?(!DOCTYPE|meta|body|html|head|title)",negate=TRUE), collapse="\n")
+  paste('\\out{', escape_rd(widg), '}\n', sep="\n")
+}
+
+#'@export
+#'@importFrom stringi stri_replace_all_fixed
+#'@noRd
+escape_rd <- function(x) {
+  stri_replace_all_fixed(
+    stri_replace_all_fixed(
+      stri_replace_all_fixed(
+        stri_replace_all_fixed(x, "\\", "\\\\"),
+        "%", "\\%"),
+      "{", "\\{"),
+    "}", "\\}")
+}
 
