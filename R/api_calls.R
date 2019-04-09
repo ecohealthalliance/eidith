0e13d2d995b88e7e01277a52566e867f574f1886
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2019-04-09 18:30:42 +0200

    Add EHP country handling.  Add error message if country name is not recognized.

--- R/api_calls.R
+++ R/api_calls.R
@@ -2,6 +2,47 @@ eidith_base_url <- "https://predict2api.eidith.org/api/modeling/"
 
 eidith2_base_url <- "https://predict2api.eidith.org/api/Extract/"
 
+event_non_ehp_cols <- c("Country", "SiteName", "ConcurrentSamplingSite", "District", "StateProv",
+                        "SiteLatitude", "SiteLongitude", "EventLatitude", "EventLongitude", "EventDate",
+                        "Season", "DurationDays", "EventName", "Recorder", "Organization", "HumanDensityImpact",
+                        "DiseaseTransmissionInterfaces", "OutbreakName", "VeterinarianCare", "SamplingAreaSize",
+                        "HumansPresent", "RodentsPresent", "BatsPresent", "NHPPresent", "BirdsPresent", "CarnivoresPresent",
+                        "UngulatesPresent", "PangolinsPresent", "PoultryPresent", "GoatsPresent", "CamelsPresent",
+                        "SwinePresent", "CattlePresent", "DogsPresent", "CatsPresent", "drinkingWaterShared",
+                        "bathingWaterShared", "ToiletsAvailable", "DrinkingWaterSource", "AverageTripToWater",
+                        "InsectVectors", "VectorControlMeasures", "CommunityEngagement", "CommunityEngagementDate",
+                        "CommunityEngagementNotes", "NbrBeds", "IsDeepForest", "Notes")
+
+human_non_ehp_cols <- c("Country", "SiteName", "ConcurrentSamplingSite", "District", "StateProv",
+                        "SiteLatitude", "SiteLongitude", "EventLatitude", "EventLongitude",
+                        "EventDate", "Season", "DurationDays", "EventName", "DiseaseTransmissionInterfaces",
+                        "GAINS4_SampleUnitID", "Q1 ParticipantID", "Q3 DateOfInterview", "Q4 BeginTimeInterview",
+                        "Q5 EndTimeInterview", "Q6 InterviewCity", "Q6 InterviewStateProv", "Q6 InterviewLatitude",
+                        "Q6 InterviewLongitude", "Q7 Gender", "Q8 Age", "Q9 LiveCity", "Q9 LiveStateProv",
+                        "Q9 LiveLatitude", "Q9 LiveLongitude", "Q10 LengthLived", "Q11 PeopleInDwelling",
+                        "Q12 ChildrenInDwelling", "Q13 MalesInDwelling", "Q14 RoomsInDwelling", "Q15 DwellingPermanentStructure?",
+                        "Q16 DrinkingWaterSource", "Q17 WaterTreated?", "Q18 WaterTreatment", "Q19 WaterUsedByAnimals?",
+                        "Q20 DedicatedLocationForWaste?", "Q21 FoodStorageContainers", "DemographicNotes", "Q22 HighestEducation",
+                        "Q23 HighestEducatinMother", "Q24 Livelihood(s)", "LivelihoodGroup(s)IfOther", "Q25 MostTimeSpent",
+                        "ModulesCompleted", "Q26 JobPosition", "Q27 WorkLocationCity", "Q27 WorkLocationProv", "Q27 WorkLocationLatitude",
+                        "Q27 WorkLocationLongitude", "LivelihoodNotes", "Q28 Treatment", "Q29 SymptomsLife", "Q30 HadSymptomsInLastYear",
+                        "Q31 SymptomsInLastYear", "Q32 SicknessCause", "Q33 SymptomsInLastYearOtherPeople", "Q34 SymptomsInLastYearOtherPeople",
+                        "Q35 IllnessDeath", "MedicalHistoryNotes", "Q36 Travelled", "Q37 More than 6", "Q37 TravelledCity Loc 1",
+                        "Q37 Latitude Loc 1", "Q37 Longitude Loc 1", "Q37 TravelledCity Loc 2", "Q37 Latitude Loc 2", "Q37 Longitude Loc 2",
+                        "Q37 TravelledCity Loc 3", "Q37 Latitude Loc 3", "Q37 Longitude Loc 3", "Q37 TravelledCity Loc 4", "Q37 Latitude Loc 4",
+                        "Q37 Longitude Loc 4", "Q37 TravelledCity Loc 5", "Q37 Latitude Loc 5", "Q37 Longitude Loc 5", "Q37 TravelledCity Loc 6",
+                        "Q37 Latitude Loc 6", "Q37 Longitude Loc 6", "Q38 Travel Reason", "MovementNotes", "Q39 PetInDwellingLife", "Q39 PetInDwellingLastYear",
+                        "Q40 HandleAnimalsLife", "Q40 HandleAnimalsLastYear", "Q41 RaisedAnimalsLife", "Q41 RaisedAnimalsLastYear", "Q42 SharedWaterLife",
+                        "Q42 SharedWaterLastYear", "Q43 AnimalFecesFoodLife", "Q43 AnimalFecesFoodLastYear", "Q44 AnimalsInFoodLife", "Q44 AnimalsInFoodLastYear",
+                        "Q45 AnimalsInDwellingLife", "Q45 AnimalsInDwellingLastYear", "Q46 CookedMeatLife", "Q46 CookedMeatLastYear", "Q47 EatenRawMeatLife",
+                        "Q47 EatenRawMeatLastYear", "Q48 EatenSickAnimalLife", "Q48 EatenSickAnimalLastYear", "Q49 EatenDeadAnimalLife", "Q49 EatenDeadAnimalLastYear",
+                        "Q50 SoldDeadAnimalLife", "Q50 SoldDeadAnimalLastYear", "Q51 ScratchedBittenLife", "Q51 ScratchedBittenLastYear", "Q52 HuntedAnimalLife",
+                        "Q52 HuntedAnimalLastYear", "Q53 SlaughtedAnimalLife", "Q53 SlaughtedAnimalLastYear", "Q54 ScratchedBittenAction", "Q55 RiskOpenWound",
+                        "Q56 Contact with rodents/shrews", "Q56 Contact with bats", "Q56 Contact with non-human primates", "Q56 Contact with birds",
+                        "Q56 Contact with carnivores", "Q56 Contact with ungulates", "Q56 Contact with pangolins", "Q56 Contact with poultry/other fowl",
+                        "Q56 Contact with goats/sheep", "Q56 Contact with camels", "Q56 Contact with swine", "Q56 Contact with cattle/buffalo", "Q56 Contact with horses",
+                        "Q56 Contact with dogs", "Q56 Contact with cats", "Q57 WorriedAboutDisease", "SpecimensCollected", "Organization")
+
 #' Return he names of the tables in the EIDITH database.
 #'
 #' @rdname endpoints
@@ -21,8 +62,7 @@ p2_api_endpoints <- function() {
     "ExtractiveIndustry", "MarketValueChain", "NaturalAreas", "WildlifeRestaurant", "ZooSanctuary",
     "Human", "HumanCropProduction", "HumanAnimalProduction","HumanExtractiveIndustry", "HumanHospitalWorker",
     "HumanHunter", "HumanMarket", "HumanRestaurant", "HumanSickPerson", "HumanTemporarySettlements", "HumanZoo",
-    "Test", "TestDataInterpreted", "TestDataSerology", "Behavioral", "Training"#,
-    #"HumanEHP", "HumanAnimalProductionEHP", "HumanHunterEHP"
+    "Test", "TestDataInterpreted", "TestDataSerology", "Behavioral", "Training"
   )
 }
 
@@ -179,14 +219,23 @@ Contact technology@eidith.org about permissions. See ?ed_contact.")
 #' @importFrom tibble as_tibble
 #' @importFrom purrr map imap map_df map_lgl
 #' @export
-ed2_get <- function(endpoint2, country=predict_countries(), postprocess=TRUE, verbose=interactive(),
-                    header_only=FALSE, lmdate_from="2000-01-01",
-                    lmdate_to=Sys.Date() + 1, auth=NULL, ...) {
+ed2_get <- function(endpoint2, country=NULL, postprocess=TRUE, verbose=interactive(),
+                    header_only=FALSE, auth=NULL, ...) {
 
-  url <- map(country, function(x){
-    modify_url(url =  paste0(eidith2_base_url, "Extract", endpoint2, "Data"),
-               query = list(country = paste0("'", x, "'")))
-  })
+  if(is.null(country)){
+    url <-  paste0(eidith2_base_url, "Extract", endpoint2, "Data")
+  } else {
+    if(any(!country %in% predict_countries())) {
+      stop(paste(
+        "Not recognized PREDICT country: ",
+        paste(country[!country %in% predict_countries()], collapse = ", ")
+        ))
+    }
+    url <- map(country, function(x){
+      modify_url(url =  paste0(eidith2_base_url, "Extract", endpoint2, "Data"),
+                 query = list(country = paste0("'", x, "'")))
+    })
+  }
 
   if(endpoint2 == "TestDataInterpreted" | endpoint2 == "TestDataSerology"){
     url <- map(url, ~gsub("Data", "", .x))
@@ -218,6 +267,18 @@ ed2_get <- function(endpoint2, country=predict_countries(), postprocess=TRUE, ve
 
   if(verbose) cat_line("Importing...")
   data <- map_df(request, ~fromJSON(content(.x, as = "text", encoding="UTF-8")))
+
+  if(endpoint2 == "Event"){ # do not include ebola names in event tbl (regardless of whether EHP country)
+    data <- data[,colnames(data) %in% event_non_ehp_cols]
+  }
+
+  if(endpoint2 %in% c("Human", "HumanAnimalProduction", "HumanHunter") &
+                      (any(!country  %in% ehp_countries()) | is.null(country))){ # do not include ebola names in human tbls unless only ehp data is downloaded
+     data <- data[,colnames(data) %in% human_non_ehp_cols]
+  } else {
+    endpoint2 <- paste0(endpoint2, "EHP") # for the metadata
+  }
+
 
   if(header_only) {
     return(data)
