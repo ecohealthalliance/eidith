ff17aec8b9855370c0d72b3422ec373ed4c689ea
Noam Ross (Travis-CI) <ross@ecohealthalliance.org>
2019-05-22 12:43:38 -0400

    Remove date and header parameters from API calls
    -  Also remove references in documentation and
       rebuild doccs

--- R/api_calls.R
+++ R/api_calls.R
@@ -93,10 +93,6 @@ create_empty_p1_table <- function(e1){
 #'
 #' @param verbose Show a progress bar and other messages?
 #' @param postprocess Should data be cleaned via [ed_process()] or returned raw?
-#' @param header_only Return only the table header.  Useful for checking if
-#' API access works
-#' @param lmdate_from filter records by earliest date, in YYYY-MM-DD format
-#' @param lmdate_to filter records by latest date, in YYYY-MM-DD format (see details)
 #' @param ... additional arguments passed to [httr::GET()]
 #' @return a [tibble][tibble::tibble()]-style data frame
 #' @rdname ed_get
@@ -106,13 +102,10 @@ create_empty_p1_table <- function(e1){
 #' @importFrom tibble as_tibble
 #' @export
 ed_get <- function(endpoint, verbose=interactive(), postprocess=TRUE,
-                   header_only=FALSE, lmdate_from="2000-01-01",
-                   lmdate_to=Sys.Date() + 1, auth=NULL, ...) {
+                   auth=NULL, ...) {
 
-  url <- modify_url(url =  paste0(eidith_base_url, endpoint),
-                    query = list(header_only = ifelse(header_only, "y", "n"),
-                                 lmdate_from = lmdate_from,
-                                 lmdate_to = lmdate_to))
+  url <- paste0(eidith_base_url, endpoint)
+
   if(verbose) {
     pbar <- progress()
     cat_line(red(paste("Downloading PREDICT-1", endpoint, "table...\n")))
@@ -138,11 +131,8 @@ Contact technology@eidith.org about permissions. See ?ed_contact.")
   if(verbose) cat_line("Importing...\n")
   data <- fromJSON(content(request, as = "text", encoding="UTF-8"))
 
-  if(header_only) {
-    return(data)
-  } else {
-    data <- as_tibble(data)
-  }
+  data <- as_tibble(data)
+
 
   if(nrow(data) == 0){
     data <- create_empty_p1_table(endpoint)
@@ -168,8 +158,6 @@ Contact technology@eidith.org about permissions. See ?ed_contact.")
 
 
 #' @param verbose Show a progress bar and other messages?
-#' @param header_only Return only the table header.  Useful for checking if
-#' API access works
 #' @param lmdate_from filter records by earliest date, in YYYY-MM-DD format
 #' @param lmdate_to filter records by latest date, in YYYY-MM-DD format (see details)
 #' @param ... additional arguments passed to [httr::GET()]
@@ -185,7 +173,7 @@ Contact technology@eidith.org about permissions. See ?ed_contact.")
 #' @importFrom purrr map imap map_df map_lgl
 #' @export
 ed2_get <- function(endpoint2, country=NULL, p1_data=FALSE, postprocess=TRUE,
-                    verbose=interactive(), header_only=FALSE, auth=NULL, ...) {
+                    verbose=interactive(), auth=NULL, ...) {
 
   # should endpoint2 have "data" appeneded in the url call?
   url_end <- ifelse(endpoint2 %in% c("TestDataInterpreted", "TestDataSerology"), "", "Data")
@@ -293,11 +281,8 @@ ed2_get <- function(endpoint2, country=NULL, p1_data=FALSE, postprocess=TRUE,
       filter(!is.na(`(Hunter Module) Q1 AnimalsHunted`))
   }
 
-  if(header_only) {
-    return(data)
-  } else {
-    data <- as_tibble(data)
-  }
+  data <- as_tibble(data)
+
 
   if(nrow(data) == 0){
     data <- create_empty_p2_table(endpoint2)
@@ -329,52 +314,37 @@ ed2_get <- function(endpoint2, country=NULL, p1_data=FALSE, postprocess=TRUE,
 
 #' @rdname ed_get
 #' @export
-ed_get_events <- function(verbose=interactive(), postprocess=TRUE,
-                          header_only=FALSE, lmdate_from="2000-01-01",
-                          lmdate_to=Sys.Date() + 1, ...) {
-  ed_get("Event", verbose, postprocess, header_only, lmdate_from, lmdate_to, ...)
+ed_get_events <- function(verbose=interactive(), postprocess=TRUE, ...) {
+  ed_get("Event", verbose, postprocess, ...)
 }
 
 
 #' @rdname ed_get
 #' @export
-ed_get_animals <- function(verbose=interactive(), postprocess=TRUE,
-                           header_only=FALSE, lmdate_from="2000-01-01",
-                           lmdate_to=Sys.Date() + 1, ...) {
-  ed_get("Animal", verbose, postprocess, header_only, lmdate_from, lmdate_to, ...)
+ed_get_animals <- function(verbose=interactive(), postprocess=TRUE, ...) {
+  ed_get("Animal", verbose, postprocess, ...)
 }
 
 #' @rdname ed_get
 #' @export
-ed_get_specimens <- function(verbose=interactive(), postprocess=TRUE,
-                             header_only=FALSE, lmdate_from="2000-01-01",
-                             lmdate_to=Sys.Date() + 1, ...) {
-  ed_get("Specimen", verbose, postprocess, header_only,
-         lmdate_from, lmdate_to, ...)
+ed_get_specimens <- function(verbose=interactive(), postprocess=TRUE, ...) {
+  ed_get("Specimen", verbose, postprocess, ...)
 }
 
 #' @rdname ed_get
 #' @export
-ed_get_tests <- function(verbose=interactive(), postprocess=TRUE,
-                         header_only=FALSE, lmdate_from="2000-01-01",
-                         lmdate_to=Sys.Date() + 1, ...) {
-  ed_get("Test", verbose, postprocess, header_only, lmdate_from, lmdate_to, ...)
+ed_get_tests <- function(verbose=interactive(), postprocess=TRUE, ...) {
+  ed_get("Test", verbose, postprocess, ...)
 }
 
 #' @rdname ed_get
 #' @export
-ed_get_viruses <- function(verbose=interactive(), postprocess=TRUE,
-                           header_only=FALSE, lmdate_from="2000-01-01",
-                           lmdate_to=Sys.Date() + 1, ...) {
-  ed_get("Virus", verbose, postprocess, header_only,
-         lmdate_from, lmdate_to, ...)
+ed_get_viruses <- function(verbose=interactive(), postprocess=TRUE, ...) {
+  ed_get("Virus", verbose, postprocess, ...)
 }
 
 #' @rdname ed_get
 #' @export
-ed_get_testspecimen <- function(verbose=interactive(), postprocess=TRUE,
-                                header_only=FALSE, lmdate_from="2000-01-01",
-                                lmdate_to=Sys.Date() + 1, ...) {
-  ed_get("TestIDSpecimenID", verbose, postprocess, header_only,
-         lmdate_from, lmdate_to, ...)
+ed_get_testspecimen <- function(verbose=interactive(), postprocess=TRUE, ...) {
+  ed_get("TestIDSpecimenID", verbose, postprocess,  ...)
 }
