68e90a08065e0eb5b7b592938df54b116f9bb640
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2020-11-09 19:10:15 -0500

    modify local download function

--- R/import_local_database.R
+++ R/import_local_database.R
@@ -10,26 +10,19 @@
 import_local_db <- function(database = c("global", "eha", "eha_with_malaysia")){
 
   # establish paths
-  drive_url <- switch(database, "global" = "https://drive.google.com/file/d/1LC_i7jba6kbwMcIa3n_MNu8iQnvF1gsP/view?usp=sharing")
+  drive_url <- switch(database, "global" = "https://drive.google.com/file/d/1BClZHx5WJV-8sO_vXyMoIvp7NfUZ2m2M/view?usp=sharing")
     #paste0("~/eidith/", database, "/eidith_db.zip")
   local_path <- file.path(rappdirs::user_data_dir(),
                           "eidith")
-  local_path_zip <- paste0(local_path,  "/eidith_db.zip")
+  local_path_zip <- paste0(local_path,  "/eidith_db.sqlite.zip")
 
   #save to eidith local share folder from google drive
   googledrive::drive_download(file = drive_url,
                  path = local_path_zip,
                  overwrite = TRUE)
 
-  # get unzip password
-  pw <- Sys.getenv(paste(toupper(database), "EIDITH", "PASSWORD", sep = "_"))
-  if(pw == ""){
-    pw <- getPass::getPass(paste0("EIDITH ", database, " password"))
-  }
-
   # unzip
-  local_path <- paste0("'", local_path, "'")
-  local_path_zip <- paste0("'", local_path_zip, "'")
-  system(command = paste0("unzip -o -P ", pw, " ", local_path_zip, " -d ", local_path), wait = TRUE )
-  return("complete")
+  unzip(zipfile = local_path_zip, exdir = local_path)
+
+  return(local_path)
 }
